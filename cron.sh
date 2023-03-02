0a5d1293b33be5782b6a63511d89a98df1294d93
Verdanatech Soluções em TI <halexsandro.sales@verdanatech.com>
2023-03-02 12:07:45 -0300

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+# -------------------------------------------------------------------------
+# @Programa
+# 	@name: cron.sh
+#	@versao: 1.0.0
+#	@Data 02 de Março de 2023
+#	@Copyright: Verdanatech Soluções em TI, 2015 - 2023
+# --------------------------------------------------------------------------
+# LICENSE
+#
+# verdanadeskGLPiBKP.sh is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# verdanadeskGLPiBKP.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------
 
 umask 0000
 
